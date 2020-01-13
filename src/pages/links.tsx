70344a8b19b2331dfcd9295f6cb069c6c834cc88
amalhanaja <amalhanaja@gmail.com>
2020-01-13 23:02:37 +0700

    Add Personal Logo

--- src/pages/links.tsx
+++ src/pages/links.tsx
@@ -5,6 +5,7 @@ import IconInstagram from "../components/Icons/Instagram"
 import IconLinkedIn from "../components/Icons/LinkedIn"
 import IconGithub from "../components/Icons/Github"
 import IconLink from "../components/Icons/IconLink"
+import Helmet from "react-helmet"
 
 const Wrapper = styled.div`
   width: 100%;
@@ -23,25 +24,46 @@ const AvatarImg = styled.img`
 `
 
 const SocialMediaWrapper = styled.div`
-    margin-top: 16px;
-    margin-bottom: 16px;
+  margin-top: 16px;
+  margin-bottom: 16px;
 `
 
 const LinksPage: React.FC = () => {
   return (
     <Wrapper>
       <GlobalStyle />
-      <AvatarImg alt="avatar" src="https://scontent-sin6-2.cdninstagram.com/v/t51.2885-19/s320x320/80603429_1307448196122574_2992266679620206592_n.jpg?_nc_ht=scontent-sin6-2.cdninstagram.com&_nc_cat=110&_nc_ohc=4wspcgtnlV0AX-ua28q&oh=e60b4412553a2dfb5642bcf854d064c9&oe=5EA801F3" />
+      <Helmet title={"Alfian Akmal Hanantio | Android Engineer"}>
+        <html lang="en" />
+      </Helmet>
+      <AvatarImg
+        alt="avatar"
+        src="https://scontent-sin6-2.cdninstagram.com/v/t51.2885-19/s320x320/80603429_1307448196122574_2992266679620206592_n.jpg?_nc_ht=scontent-sin6-2.cdninstagram.com&_nc_cat=110&_nc_ohc=4wspcgtnlV0AX-ua28q&oh=e60b4412553a2dfb5642bcf854d064c9&oe=5EA801F3"
+      />
       <h2>Alfian Akmal Hanantio</h2>
       <p>Android Engineer</p>
       <SocialMediaWrapper>
-        <IconLink rel="nofollow noopener noreferrer" aria-label="Instagram" href="https://instagram.com/amal.codes/" target="_blank">
+        <IconLink
+          rel="nofollow noopener noreferrer"
+          aria-label="Instagram"
+          href="https://instagram.com/amal.codes/"
+          target="_blank"
+        >
           <IconInstagram />
         </IconLink>
-        <IconLink rel="nofollow noopener noreferrer" aria-label="LinkedIn" href="https://linkedin.com/in/amalhanaja" target="_blank">
+        <IconLink
+          rel="nofollow noopener noreferrer"
+          aria-label="LinkedIn"
+          href="https://linkedin.com/in/amalhanaja"
+          target="_blank"
+        >
           <IconLinkedIn />
         </IconLink>
-        <IconLink rel="nofollow noopener noreferrer" aria-label="Github" href="https://github.com/amalhanaja" target="_blank">
+        <IconLink
+          rel="nofollow noopener noreferrer"
+          aria-label="Github"
+          href="https://github.com/amalhanaja"
+          target="_blank"
+        >
           <IconGithub />
         </IconLink>
       </SocialMediaWrapper>
