69dfd53ae815b57773f1bf46f6893bdbb6e8c527
amalhanaja <amalhanaja@gmail.com>
2020-02-12 03:22:52 +0700

    GlobalStyle: set font-size and remove unused import font-weight

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,7 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700&display=swap');
+  @import url('https://fonts.googleapis.com/css?family=Quicksand:400,500,700&display=swap');
   html {
     box-sizing: border-box;
     width: 100%;
@@ -10,6 +10,7 @@ const GlobalStyle = createGlobalStyle`
   *::before,
   *::after {
     box-sizing: inherit;
+    outline: none;
   }
   
   h1, h2, h3, h4, h5, h6 {
@@ -24,8 +25,9 @@ const GlobalStyle = createGlobalStyle`
     width: 100%;
     min-height: 100vh;
     margin: 0;
+    padding: 0;
+    font-size: ${(props): string => props.theme.fontSizes.base};
     overflow-x: hidden;
-    background: #f2f2f2;
     font-family: Quicksand, San Francisco, SF Pro Text, -apple-system, system-ui, BlinkMacSystemFont, Roboto, Helvetica Neue, Segoe UI, Arial, sans-serif;
   }
 `;
