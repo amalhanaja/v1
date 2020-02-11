714f2c57240e0b8d9172a8af4de230a6bd836177
amalhanaja <amalhanaja@gmail.com>
2020-02-12 03:21:30 +0700

    Styled Components: Add Theme Declaration

---
+++ src/styles/styled-components.d.ts
@@ -0,0 +1,7 @@
+import {} from 'styled-components';
+import { Theme } from './themes/types';
+
+declare module 'styled-components' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  export interface DefaultTheme extends Theme {}
+}
