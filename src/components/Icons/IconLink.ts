546aa1d7f6e2ef5b4364f8e1a586e89954ec22d1
amalhanaja <amalhanaja@gmail.com>
2020-02-13 05:07:44 +0700

    IconLink: apply theme

--- src/components/Icons/IconLink.ts
+++ src/components/Icons/IconLink.ts
@@ -3,21 +3,21 @@ import styled from 'styled-components';
 const IconLink = styled.a`
   cursor: pointer;
   padding: 12px;
-  fill: #333333;
+  fill: ${(props): string => props.theme.colors.text};
   border-radius: 50%;
-  background: linear-gradient(145deg, #ffffff, #dcdcdc);
-  box-shadow: 4px 4px 8px #cfcfcf, -4px -4px 8px #ffffff;
+  background: ${(props): string => `linear-gradient(145deg, ${props.theme.colors.surfaceLight}, ${props.theme.colors.surfaceDark})`};
+  box-shadow: ${(props): string => `4px 4px 8px ${props.theme.colors.surfaceDark}, -4px -4px 8px ${props.theme.colors.surfaceLight}`};
   display: flex;
   transition: all 0.2s ease-out;
   &:hover,
   &:focus {
-    fill: #19a4e1;
+    fill: ${({ theme }): string => theme.colors.primary};
   }
   &:hover {
     transform: scale(1.1);
   }
   &:focus {
-    background: linear-gradient(145deg, #dcdcdc, #ffffff);
+    background: ${({ theme }): string => `linear-gradient(145deg, ${theme.colors.surfaceDark}, ${theme.colors.surfaceLight})`};
     transform: scale(0.95);
   }
   svg {
