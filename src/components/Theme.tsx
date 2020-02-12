10043c31e222f6b050d5898af476e6354f4c6e52
amalhanaja <amalhanaja@gmail.com>
2020-02-13 06:41:33 +0700

    Build Error: Fix Webpack Error caused window and localStorage

--- src/components/Theme.tsx
+++ src/components/Theme.tsx
@@ -9,16 +9,19 @@ interface ThemeProps {
   children: React.ReactNode;
 }
 const Theme: React.FC<ThemeProps> = ({ children }) => {
-  const mql = window.matchMedia('(prefers-color-scheme: dark');
-  const [themeName, setThemeName] = React.useState(
-    localStorage.getItem('theme') || mql.matches
-      ? DarkTheme.themeType
-      : LightTheme.themeType,
-  );
+  const [themeName, setThemeName] = React.useState(LightTheme.themeType);
   const toggleTheme = (themeType: ThemeType): void => {
     localStorage.setItem('theme', themeType);
     setThemeName(themeType);
   };
+
+  React.useEffect(() => {
+    const mql = window.matchMedia('(prefers-color-scheme: dark');
+    const themeType = localStorage.getItem('theme') || mql.matches
+      ? DarkTheme.themeType
+      : LightTheme.themeType;
+    setThemeName(themeType);
+  }, []);
 
   const theme = themeName === DarkTheme.themeType ? DarkTheme : LightTheme;
   return (
